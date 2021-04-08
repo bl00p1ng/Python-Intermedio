d2cf81776122017bd69f49d5f3d8c6b7d3a61547
bl00p1ng <blooping@protonmail.com>
2021-04-08 18:06:07 -0500

    Reto clase 10

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,3 +1,5 @@
+import math
+
 def run():
     # my_dict = {}
     # for i in range(1, 101):
@@ -5,7 +7,11 @@ def run():
     #         my_dict[i] = i**3
 
     my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
-    print(my_dict)
+    print(my_dict, "\n")
+
+    # RETO DE LA CLASE
+    challenge = {i: math.sqrt(i) for i in range(1, 1001)}
+    print(challenge)
 
 
 if __name__ == '__main__':
